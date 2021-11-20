28832ffd92c5c58289a4c32c8da54836b0e0328e
Natalia Katsiapi <kat26natalia@gmail.com>
2021-11-20 20:52:50 +0200

    Finish productions

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 # def test(sth=8): 
 # 	print 4
-print max(x.fr(test))
+print max(x.fr(test), 5)
 print max(x.t(), 2)
 print 7 ** t()
 
@@ -10,9 +10,29 @@ fr()
 print [4**5,5,2]
 print len([4**5,5,2])
 print len(6**6)
-
+print something[2**7]
+print 8 ** max(3, 8)
 # fr(8)
 # fr(5634)
+
+print 563.93 / 2 * 3 ** 5
+
+print 563.93 / testf(2 * 3) ** 5
+
+print 563.93 ** 2 * 3
+
+print 563.93 / testf(2 * 3, [y, x, w] ** q) ** 5
+
+testf(2* 3 ,[y, x, w] ** k)
+
+assert min(x.k(9))
+
+return 7 + 8 * 9
+return 7 * 8 + 9
+
+
+# print 563.93 / 2 * 3 ** 5
+
 
 # x = 23 ** rgi
 
