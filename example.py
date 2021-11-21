7b25e2a87e5a86f3eccb11bd7ad5895193e7b213
AthinaDavari <t8180020@aueb.gr>
2021-11-21 02:23:16 +0200

    Add examples

--- example.py
+++ example.py
@@ -25,15 +25,48 @@ print 563.93 / testf(2 * 3, [y, x, w] ** q) ** 5
 
 testf(2* 3 ,[y, x, w] ** k)
 
-assert min(x.k(9))
+
+# print 563.93 / 2 * 3 ** 5
 
 return 7 + 8 * 9
 return 7 * 8 + 9
 
+print x
 
-# print 563.93 / 2 * 3 ** 5
 
+assert min(x.k(9))
 
 # x = 23 ** rgi
 
 # fr(7)
+
+if true :
+    return 2 / 5
+
+
+for Identifier in kk : 
+    x = 78
+
+k[1] = len([5,6])
+
+df('fg',rte,67)
+
+x -= min(1,3)
+
+x /= 7
+
+print "wee areee minipython masters"
+
+litresOfCoffee = 15
+
+while litresOfCoffee>0 :
+    code("compilers assignment")
+    litresOfCoffee -= 1
+
+while 8 >= 9 :
+    x = 78
+
+while true and not false:
+    endlessLoop("we are screwed")
+
+x -= 1 +1
