35b474e985a757af3a31f608a8d610f27ded891b
AthinaDavari <t8180020@aueb.gr>
2021-12-28 21:33:56 +0200

    Create new grammar with ast and add example file

---
+++ example2.py
@@ -0,0 +1,9 @@
+x = 3+2*4+2+1
+assert 9, 5+5
+if y > 7 or not x > 8 and y <= 3 and not false:
+print _id_, 4
+x -= min(1,3)
+test(x + 10)
+test(x8, y0, [3, 5, 7])
+import library.specific as l
+from  y import x,b as r532
