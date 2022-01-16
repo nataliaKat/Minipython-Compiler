f0e4162da2e8798f9293783b0a78d8d4a6dc3d36
gdrosos <t8180024@aueb.gr>
2022-01-16 21:40:30 +0200

    Remove SymbolTableFiler Class

--- ParserTest2.java
+++ ParserTest2.java
@@ -8,63 +8,38 @@ import minipython.parser.Parser;
 import minipython.node.Start;
 
 
-public class ParserTest2
-{
-  public static void main(String[] args)
-  {
-    try
-    {
-      Parser parser = 
-        new Parser(
-        new Lexer(
-        new PushbackReader(
-        new FileReader(args[0].toString()), 1024)));
+public class ParserTest2 {
+    public static void main(String[] args) {
+        try {
+            Parser parser =
+                    new Parser(
+                            new Lexer(
+                                    new PushbackReader(
+                                            new FileReader(args[0].toString()), 1024)));
 
-      Start ast = parser.parse();
+            Start ast = parser.parse();
 
-	  ast.apply(new ASTPrinter());
-      Hashtable<String, Variable> variables = new Hashtable();
-      Hashtable<String, List<Function>> functions =  new Hashtable(); /* List of functions for overloading */
-//      ast.apply(new SymbolTableFiller(symtable));
-      System.out.println("Entering First Visitor:");
-      ast.apply(new Visitor1(functions));
-      System.out.println("Functions");
-      System.out.println(functions);
-      System.out.println("Variables");
-      for (Map.Entry<String, Variable> entry: variables.entrySet()) {
-        String key = entry.getKey();
-        Variable value = entry.getValue();
+            ast.apply(new ASTPrinter());
+            Hashtable<String, Variable> variables = new Hashtable();
+            Hashtable<String, List<Function>> functions = new Hashtable(); /* List of functions for overloading */
+            System.out.println("Entering First Visitor:");
+            ast.apply(new Visitor1(functions));
+            System.out.println("Entering Second Visitor:");
+            ast.apply(new Visitor2(variables, functions));
+            System.out.println("Entering Third Visitor:");
+            ast.apply(new Visitor3(variables, functions));
+            System.out.println("Functions");
+            System.out.println(functions);
+            System.out.println("Variables");
+            for (Map.Entry<String, Variable> entry : variables.entrySet()) {
+                String key = entry.getKey();
+                Variable value = entry.getValue();
 
-        System.out.println("Key: " + key + " Value: " + value);
-      }
-      System.out.println("Entering Second Visitor:");
-      ast.apply(new Visitor2(variables, functions));
-      System.out.println("Functions");
-      System.out.println(functions);
-      System.out.println("Variables");
-      for (Map.Entry<String, Variable> entry: variables.entrySet()) {
-        String key = entry.getKey();
-        Variable value = entry.getValue();
-
-        System.out.println("Key: " + key + " Value: " + value);
-      }
-      System.out.println("Entering Third Visitor:");
-      ast.apply(new Visitor3(variables, functions));
-      System.out.println("Functions");
-      System.out.println(functions);
-      System.out.println("Variables");
-      for (Map.Entry<String, Variable> entry: variables.entrySet()) {
-        String key = entry.getKey();
-        Variable value = entry.getValue();
-
-        System.out.println("Key: " + key + " Value: " + value);
-      }
-//      System.out.println(ast);
+                System.out.println("Key: " + key + " Value: " + value);
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+        }
     }
-    catch (Exception e)
-    {
-      System.err.println(e);
-    }
-  }
 }
 
